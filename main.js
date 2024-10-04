041252810d336f994e72a896bf285f619d075eff
KiranManaware <kiranmanaware123@gamil.com>
2024-10-04 10:47:23 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+let button=document.querySelector("button");
+let card=document.querySelector(".card");
+let input=document.querySelector("input");
+let select=document.querySelector("select");
+let form=document.querySelector("form");
+let h1=document.querySelector("h1")
+// console.log(button);
+// console.log(card);
+// console.log(input);
+// console.log(select);
+// console.log(form);
+// console.log(h1);
+
+
+//Events
+const runEvent=(e)=>{
+    // console.log("Button Clicked");
+    // console.log(e);
+    // console.log(e.type);
+    // console.log(e.target);
+    // console.log(e.target.className);
+    // console.log(e.target.id);    
+    // console.log(e.altKey);    
+    // console.log(e.ctrlKey);
+    // console.log(e.shiftKey);
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+    // console.log("Event : "+e.type); 
+    // console.log(e.clientX,e.clientY); 
+    // card.style.backgroundColor=`rgb(${e.clientX},${e.clientY},140)`
+    console.log("Event : "+e.type); 
+    input.style.color="yellow"
+    e.preventDefault();
+};
+// button.addEventListener("click",runEvent);
+// button.addEventListener("dblclick",runEvent);  //It is not working in my system
+// button.addEventListener("mousedown",runEvent);
+// button.addEventListener("mouseup",runEvent);
+// button.addEventListener("mouseover",runEvent);
+// card.addEventListener("mouseup",runEvent);
+// card.addEventListener("mouseover",runEvent);
+// card.addEventListener("mouseout",runEvent)
+// card.addEventListener("mouseenter",runEvent)
+// card.addEventListener("mouseleave",runEvent)
+// card.addEventListener("mousemove",runEvent)
+// input.addEventListener("input",runEvent)
+// input.addEventListener("change",runEvent)
+// select.addEventListener("change",runEvent)
+// input.addEventListener("focus",runEvent)
+// input.addEventListener("blur",runEvent)
+// input.addEventListener("cut",runEvent)
+// input.addEventListener("copy",runEvent)
+// input.addEventListener("paste",runEvent)
+// input.addEventListener("keydown",runEvent)
+// input.addEventListener("keyup",runEvent)
+// input.addEventListener("keypress",runEvent)
+
+
+// form.addEventListener("submit",runEvent)
+
+
+// change
+// click
+// submit
+// mouseover+
+
+
+  
